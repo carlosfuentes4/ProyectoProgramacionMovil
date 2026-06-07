277c63ef102441475b7e11c8c5510529fb0fe5d1
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-06-06 23:11:24 -0600

    carlos: agruegue el componente para los cards y los implement en homescreen

--- MiApp/src/screens/HomeScreen.tsx
+++ MiApp/src/screens/HomeScreen.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../navigation/StackNavigator';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/StackNavigator";
+import RecipeCard from "../components/RecipeCards";
 
-
-type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type Props = NativeStackScreenProps<RootStackParamList, "Home">;
 
 export default function HomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>¡Bienvenido a la App!</Text>
-      <Button 
-        title="Ir al Login" 
-        onPress={() => navigation.navigate('Login')} // Apunta a Login
+
+      <RecipeCard
+        title={"Receta 1"}
+        description={"Receta de prueba"}
+        imageUrl={"https://unsplash.com/es/fotos/verduras-y-carne-en-cuenco-kcA-c3f_3FE"}
+        onPress={()=>{console.log("card 1")}}
+      />
+      <RecipeCard
+        title={"Receta 2"}
+        description={"Receta de prueba"}
+        imageUrl={"https://img.magnific.com/foto-gratis/primer-plano-carne-asada-salsa-verduras-patatas-fritas-plato-sobre-mesa_181624-35847.jpg?semt=ais_hybrid&w=740&q=80"}
+        onPress={()=>{console.log("card 2")}}
+      />
+
+      <Button
+        title="Ir al Login"
+        onPress={() => navigation.navigate("Login")} // Apunta a Login
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-  text: { fontSize: 20, marginBottom: 20, fontWeight: 'bold' }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: { fontSize: 20, marginBottom: 20, fontWeight: "bold" },
 });
