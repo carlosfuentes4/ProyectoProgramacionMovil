f30ea84d968542d4a432f0cbf7b7d1a85ba60044
carlosfuentes4 <carlosfuent0059@gmail.com>
2026-06-07 23:09:29 -0600

    resolvi conflictos entrre el merge de mi rama

--- MiApp/src/navigation/StackNavigator.tsx
+++ MiApp/src/navigation/StackNavigator.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
+import { useAuth } from '../context/AuthContext';
+import HomeScreen from '../screens/HomeScreen';
 
 export type RootStackParamList = {
   Login: undefined;
-  Main: undefined; // Cambiamos 'Home' por 'Main'
+  Home: undefined; // Cambiamos 'Home' por 'Main'
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
